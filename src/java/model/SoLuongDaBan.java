9e3cd2833e30b80699a7f1d58c50f03030054a09
HuyenNKHE <157096640+HuyenNKHE@users.noreply.github.com>
2024-02-29 14:07:54 +0700

    Update SoLuongDaBan.java

--- src/java/model/SoLuongDaBan.java
+++ src/java/model/SoLuongDaBan.java
@@ -2,29 +2,29 @@ package model;
 
 public class SoLuongDaBan {
 	  private int productID;
-	  private int soLuongDaBan;
+	  private int daban;
 	public int getProductID() {
 		return productID;
 	}
-	public SoLuongDaBan(int productID, int soLuongDaBan) {
+	public daban(int productID, int daban) {
 	
 		this.productID = productID;
-		this.soLuongDaBan = soLuongDaBan;
+		this.daban = daban;
 	}
-	public SoLuongDaBan() {
+	public daban() {
 		
 	}
 	@Override
 	public String toString() {
-		return "SoLuongDaBan [productID=" + productID + ", soLuongDaBan=" + soLuongDaBan + "]";
+		return "daban [productID=" + productID + ", daban=" + daban + "]";
 	}
 	public void setProductID(int productID) {
 		this.productID = productID;
 	}
-	public int getSoLuongDaBan() {
-		return soLuongDaBan;
+	public int getdaban() {
+		return daban;
 	}
-	public void setSoLuongDaBan(int soLuongDaBan) {
-		this.soLuongDaBan = soLuongDaBan;
+	public void setdaban(int daban) {
+		this.daban = daban;
 	}
 }
